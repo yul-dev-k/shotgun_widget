787c5758b9ffff146f80807dddf9916c3595e6fc
yul-dev-k <yul.dev.k@gmail.com>
2023-11-25 23:00:35 +0900

    Feat: checkbox 선택시 해당 isChecked 확인 기능 추가

--- widget_test.py
+++ widget_test.py
@@ -36,7 +36,8 @@ class ShotGunRetakeWidget(QWidget):
         hbox.addWidget(self.toggle_button)
 
         layout.addLayout(hbox)
-        layout.addWidget(self.table())
+        self.table()
+        layout.addWidget(self.tableWidget)
         self.setLayout(layout)
 
         self.move(300, 300)
@@ -58,65 +59,70 @@ class ShotGunRetakeWidget(QWidget):
     def table(self):
         view = self.my_retake_ui() if self.show_my_retake else self.all_retake_ui()
         total_shot = len(view)
-        self.checkboxes = []
 
-        self.table = QTableWidget()
-        self.table.setColumnCount(len(self.col_title))
-        self.table.setHorizontalHeaderLabels(self.col_title)
-        self.table.setRowCount(total_shot)
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setColumnCount(len(self.col_title))
+        self.tableWidget.setHorizontalHeaderLabels(self.col_title)
+        self.tableWidget.setRowCount(total_shot)
 
         for row_idx, retake in enumerate(view):
             checkbox = QCheckBox()
-            self.checkboxes.append(checkbox)
-            self.table.setCellWidget(row_idx, 0, checkbox)
-            self.table.setItem(row_idx, 1, QTableWidgetItem(retake['code']))
-            self.table.setItem(row_idx, 2, QTableWidgetItem(
+            checkbox.stateChanged.connect(
+                lambda state, row=row_idx: self.checkbox_state_changed(row, state))
+
+            self.tableWidget.setCellWidget(row_idx, 0, checkbox)
+            self.tableWidget.setItem(
+                row_idx, 1, QTableWidgetItem(retake['code']))
+            self.tableWidget.setItem(row_idx, 2, QTableWidgetItem(
                 retake['assigned'][0]['name']))
 
             note_item = QTableWidgetItem(retake['note'])
-            self.table.setItem(row_idx, 3, note_item)
+            self.tableWidget.setItem(row_idx, 3, note_item)
 
-            self.table.setItem(row_idx, 4, QTableWidgetItem(retake['img']))
-            self.table.setItem(
+            self.tableWidget.setItem(
+                row_idx, 4, QTableWidgetItem(retake['img']))
+            self.tableWidget.setItem(
                 row_idx, 5, QTableWidgetItem(retake['retake_v']))
 
             note_item.setToolTip(retake['note'])
 
-            self.table.setRowHeight(row_idx, self.row_height)
+            self.tableWidget.setRowHeight(row_idx, self.row_height)
 
-        self.table.setColumnWidth(0, self.col1_w)
-        self.table.setColumnWidth(1, self.col2_w)
-        self.table.setColumnWidth(2, self.col3_w)
-        self.table.setColumnWidth(3, self.col4_w)
-        self.table.setColumnWidth(4, self.col5_w)
-        self.table.setColumnWidth(5, self.col6_w)
-
-        return self.table
+        self.tableWidget.setColumnWidth(0, self.col1_w)
+        self.tableWidget.setColumnWidth(1, self.col2_w)
+        self.tableWidget.setColumnWidth(2, self.col3_w)
+        self.tableWidget.setColumnWidth(3, self.col4_w)
+        self.tableWidget.setColumnWidth(4, self.col5_w)
+        self.tableWidget.setColumnWidth(5, self.col6_w)
 
     def update_table(self):
-        self.table.clearContents()
-        self.table.setRowCount(0)
+        self.tableWidget.clearContents()
+        self.tableWidget.setRowCount(0)
         new_view = self.my_retake_ui() if self.show_my_retake else self.all_retake_ui()
-        self.table.setRowCount(len(new_view))
+        self.tableWidget.setRowCount(len(new_view))
 
         for row_idx, retake in enumerate(new_view):
             checkbox = QCheckBox()
-            self.checkboxes.append(checkbox)
-            self.table.setCellWidget(row_idx, 0, checkbox)
-            self.table.setItem(row_idx, 1, QTableWidgetItem(retake['code']))
-            self.table.setItem(row_idx, 2, QTableWidgetItem(
+            checkbox.stateChanged.connect(
+                lambda state, row=row_idx: self.checkbox_state_changed(row, state))
+
+            self.tableWidget.setCellWidget(row_idx, 0, checkbox)
+            self.tableWidget.setItem(
+                row_idx, 1, QTableWidgetItem(retake['code']))
+            self.tableWidget.setItem(row_idx, 2, QTableWidgetItem(
                 retake['assigned'][0]['name']))
 
             note_item = QTableWidgetItem(retake['note'])
-            self.table.setItem(row_idx, 3, note_item)
+            self.tableWidget.setItem(row_idx, 3, note_item)
 
-            self.table.setItem(row_idx, 4, QTableWidgetItem(retake['img']))
-            self.table.setItem(
+            self.tableWidget.setItem(
+                row_idx, 4, QTableWidgetItem(retake['img']))
+            self.tableWidget.setItem(
                 row_idx, 5, QTableWidgetItem(retake['retake_v']))
 
             note_item.setToolTip(retake['note'])
 
-            self.table.setRowHeight(row_idx, self.row_height)
+            self.tableWidget.setRowHeight(row_idx, self.row_height)
 
     def update_toggle_button_text(self):
         if self.show_my_retake:
@@ -133,28 +139,36 @@ class ShotGunRetakeWidget(QWidget):
         QMessageBox.information(self, '알림', '업데이트가 완료되었습니다.')
 
     def update_table_data(self, data):
-        self.table.clearContents()
-        self.table.setRowCount(0)
-        self.table.setRowCount(len(data))
+        self.tableWidget.clearContents()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setRowCount(len(data))
 
         for row_idx, retake in enumerate(data):
             checkbox = QCheckBox()
-            self.checkboxes.append(checkbox)
-            self.table.setCellWidget(row_idx, 0, checkbox)
-            self.table.setItem(row_idx, 1, QTableWidgetItem(retake['code']))
-            self.table.setItem(row_idx, 2, QTableWidgetItem(
+            checkbox.stateChanged.connect(
+                lambda state, row=row_idx: self.checkbox_state_changed(row, state))
+
+            self.tableWidget.setCellWidget(row_idx, 0, checkbox)
+            self.tableWidget.setItem(
+                row_idx, 1, QTableWidgetItem(retake['code']))
+            self.tableWidget.setItem(row_idx, 2, QTableWidgetItem(
                 retake['assigned'][0]['name']))
 
             note_item = QTableWidgetItem(retake['note'])
-            self.table.setItem(row_idx, 3, note_item)
+            self.tableWidget.setItem(row_idx, 3, note_item)
 
-            self.table.setItem(row_idx, 4, QTableWidgetItem(retake['img']))
-            self.table.setItem(
+            self.tableWidget.setItem(
+                row_idx, 4, QTableWidgetItem(retake['img']))
+            self.tableWidget.setItem(
                 row_idx, 5, QTableWidgetItem(retake['retake_v']))
 
             note_item.setToolTip(retake['note'])
 
-            self.table.setRowHeight(row_idx, self.row_height)
+            self.tableWidget.setRowHeight(row_idx, self.row_height)
+
+    def checkbox_state_changed(self, row_idx, state):
+        if state == Qt.Checked:
+            print(f"현재 {row_idx} 선택")
 
 
 if __name__ == '__main__':
