23be409acbc4cf4b5c3d13f7238fac8b2593cdd2
yul-dev-k <yul.dev.k@gmail.com>
2023-11-25 21:58:45 +0900

    Feat: 데이터 업데이트 해주는 새로 고침 버튼 추가

--- widget_test.py
+++ widget_test.py
@@ -10,7 +10,7 @@ class ShotGunRetakeWidget(QWidget):
         super().__init__()
 
         self.col_title = ['✅', 'code', 'assigned', 'note', 'img', 'retake']
-        self.show_my_retake = True  # To track whether to show My Retake or All Retake
+        self.show_my_retake = True
         self.initUI()
         self.setWindowTitle('Shotgun Retake Widget')
         self.setWindowIcon(QIcon('icons.png'))
@@ -20,12 +20,19 @@ class ShotGunRetakeWidget(QWidget):
         vbox = QVBoxLayout()
         layout = QVBoxLayout()
 
+        self.refresh_button = QPushButton('새로 고침', self)
+        self.refresh_button.clicked.connect(self.refresh_data)
+        self.refresh_button.setMaximumWidth(100)
+
         self.toggle_button = QPushButton('모든 리테이크 보기', self)
-        self.toggle_button.setCheckable(True)
         self.toggle_button.clicked.connect(self.toggle_view)
         self.toggle_button.setMaximumWidth(150)
 
-        layout.addWidget(self.toggle_button, alignment=Qt.AlignRight)
+        hbox.addStretch(3)
+        hbox.addWidget(self.refresh_button)
+        hbox.addWidget(self.toggle_button)
+
+        layout.addLayout(hbox)
         layout.addWidget(self.table())
         self.setLayout(layout)
 
@@ -100,6 +107,31 @@ class ShotGunRetakeWidget(QWidget):
         else:
             self.toggle_button.setText('내 리테이크 보기')
 
+    def refresh_data(self):
+        if self.show_my_retake:
+            self.update_table_data(self.my_retake_ui())
+        else:
+            self.update_table_data(self.all_retake_ui())
+
+        QMessageBox.information(self, '알림', '업데이트가 완료되었습니다.')
+
+    def update_table_data(self, data):
+        self.table.clearContents()
+        self.table.setRowCount(0)
+        self.table.setRowCount(len(data))
+
+        for row_idx, retake in enumerate(data):
+            checkbox = QCheckBox()
+            self.checkboxes.append(checkbox)
+            self.table.setCellWidget(row_idx, 0, checkbox)
+            self.table.setItem(row_idx, 1, QTableWidgetItem(retake['code']))
+            self.table.setItem(row_idx, 2, QTableWidgetItem(
+                retake['assigned'][0]['name']))
+            self.table.setItem(row_idx, 3, QTableWidgetItem(retake['note']))
+            self.table.setItem(row_idx, 4, QTableWidgetItem(retake['img']))
+            self.table.setItem(
+                row_idx, 5, QTableWidgetItem(retake['retake_v']))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -110,7 +142,7 @@ if __name__ == '__main__':
 
 """ 
   해야할 것
-  1. 맨 처음 col에 checkbox (✅) 추가
+  1. 맨 처음 col에 checkbox (✅) 추가 
   2. 내 retake만 보는걸 기본 뷰로 ✅
   3. My Retake | All Retake view 선택 버튼 필요 ✅
   4. 기본 위젯 높이 조금 줄이기 ✅
@@ -118,5 +150,5 @@ if __name__ == '__main__':
   6. Img탭 이미지 띄우기 (클릭하면 더 크게 볼 수 있으면 좋겠음. modal 처럼)
   7. retake_v 부분 감독님 | 편집팀 그냥 이렇게 나누는게 좋을 것 같음. ✅
   8. note 마우스 hover하면 전체 글 나오게
-  9. retake 새로고침 버튼 필요
+  9. retake 새로고침 버튼 필요 ✅
 """
