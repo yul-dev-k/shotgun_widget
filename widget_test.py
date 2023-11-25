30da6830a2be9a6347589e01e81425046114402f
yul-dev-k <yul.dev.k@gmail.com>
2023-11-25 21:39:30 +0900

    Feat: 내 리테이만 보기, 모든 리테이크 보기 버튼 추가

--- widget_test.py
+++ widget_test.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 from retake import Retake
 
 
@@ -9,18 +10,28 @@ class ShotGunRetakeWidget(QWidget):
         super().__init__()
 
         self.col_title = ['✅', 'code', 'assigned', 'note', 'img', 'retake']
+        self.show_my_retake = True  # To track whether to show My Retake or All Retake
         self.initUI()
         self.setWindowTitle('Shotgun Retake Widget')
         self.setWindowIcon(QIcon('icons.png'))
 
     def initUI(self):
-
+        hbox = QHBoxLayout()
+        vbox = QVBoxLayout()
         layout = QVBoxLayout()
-        layout.addWidget(self.table(self.my_retake_ui(), self.col_title))
+
+        self.toggle_button = QPushButton('모든 리테이크 보기', self)
+        self.toggle_button.setCheckable(True)
+        self.toggle_button.clicked.connect(self.toggle_view)
+        self.toggle_button.setMaximumWidth(150)
+
+        layout.addWidget(self.toggle_button, alignment=Qt.AlignRight)
+        layout.addWidget(self.table())
         self.setLayout(layout)
 
         self.move(300, 300)
         self.setGeometry(300, 100, 570, 200)
+        self.update()
         self.show()
 
     def all_retake_ui(self):
@@ -29,27 +40,30 @@ class ShotGunRetakeWidget(QWidget):
     def my_retake_ui(self):
         return Retake().get_my_retake()
 
-    def table(self, view, col_title):
+    def toggle_view(self):
+        self.show_my_retake = not self.show_my_retake
+        self.update_table()
+        self.update_toggle_button_text()
+
+    def table(self):
+        view = self.my_retake_ui() if self.show_my_retake else self.all_retake_ui()
         total_shot = len(view)
         self.checkboxes = []
 
         self.table = QTableWidget()
-        self.table.setColumnCount(len(col_title))
-        self.table.setHorizontalHeaderLabels(col_title)
+        self.table.setColumnCount(len(self.col_title))
+        self.table.setHorizontalHeaderLabels(self.col_title)
         self.table.setRowCount(total_shot)
 
         for row_idx, retake in enumerate(view):
             checkbox = QCheckBox()
             self.checkboxes.append(checkbox)
             self.table.setCellWidget(row_idx, 0, checkbox)
-            self.table.setItem(
-                row_idx, 1, QTableWidgetItem(retake['code']))
+            self.table.setItem(row_idx, 1, QTableWidgetItem(retake['code']))
             self.table.setItem(row_idx, 2, QTableWidgetItem(
                 retake['assigned'][0]['name']))
-            self.table.setItem(
-                row_idx, 3, QTableWidgetItem(retake['note']))
-            self.table.setItem(
-                row_idx, 4, QTableWidgetItem(retake['img']))
+            self.table.setItem(row_idx, 3, QTableWidgetItem(retake['note']))
+            self.table.setItem(row_idx, 4, QTableWidgetItem(retake['img']))
             self.table.setItem(
                 row_idx, 5, QTableWidgetItem(retake['retake_v']))
 
@@ -62,18 +76,43 @@ class ShotGunRetakeWidget(QWidget):
 
         return self.table
 
+    def update_table(self):
+        self.table.clearContents()
+        self.table.setRowCount(0)
+        new_view = self.my_retake_ui() if self.show_my_retake else self.all_retake_ui()
+        self.table.setRowCount(len(new_view))
+
+        for row_idx, retake in enumerate(new_view):
+            checkbox = QCheckBox()
+            self.checkboxes.append(checkbox)
+            self.table.setCellWidget(row_idx, 0, checkbox)
+            self.table.setItem(row_idx, 1, QTableWidgetItem(retake['code']))
+            self.table.setItem(row_idx, 2, QTableWidgetItem(
+                retake['assigned'][0]['name']))
+            self.table.setItem(row_idx, 3, QTableWidgetItem(retake['note']))
+            self.table.setItem(row_idx, 4, QTableWidgetItem(retake['img']))
+            self.table.setItem(
+                row_idx, 5, QTableWidgetItem(retake['retake_v']))
+
+    def update_toggle_button_text(self):
+        if self.show_my_retake:
+            self.toggle_button.setText('모든 리테이크 보기')
+        else:
+            self.toggle_button.setText('내 리테이크 보기')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mywindow = ShotGunRetakeWidget()
     mywindow.show()
-    app.exec_()
+    sys.exit(app.exec_())
+
 
 """ 
   해야할 것
   1. 맨 처음 col에 checkbox (✅) 추가
-  2. 내 retake만 보는걸 기본 뷰로
-  3. My Retake | All Retake view 선택 버튼 필요
+  2. 내 retake만 보는걸 기본 뷰로 ✅
+  3. My Retake | All Retake view 선택 버튼 필요 ✅
   4. 기본 위젯 높이 조금 줄이기 ✅
   5. 각 col마다 width 지정해주기 ✅
   6. Img탭 이미지 띄우기 (클릭하면 더 크게 볼 수 있으면 좋겠음. modal 처럼)
